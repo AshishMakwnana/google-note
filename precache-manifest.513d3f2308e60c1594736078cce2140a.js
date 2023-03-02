56a197d85a415d071c38e7f7dda85a16d7b38d24
Ashish Makwana <ashishmakwana435@gmail.com>
2023-03-02 15:57:33 +0530

    Updates

---
+++ precache-manifest.513d3f2308e60c1594736078cce2140a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9cf8d1e820d70a5e632",
+    "url": "/google-note/static/css/main.81576c40.chunk.css"
+  },
+  {
+    "revision": "d9cf8d1e820d70a5e632",
+    "url": "/google-note/static/js/main.d9cf8d1e.chunk.js"
+  },
+  {
+    "revision": "6f6f4986655de22842af",
+    "url": "/google-note/static/js/1.6f6f4986.chunk.js"
+  },
+  {
+    "revision": "ba8b5876ad8c76e89c4a",
+    "url": "/google-note/static/js/2.ba8b5876.chunk.js"
+  },
+  {
+    "revision": "93b8460f55a4250961da",
+    "url": "/google-note/static/js/runtime~main.93b8460f.js"
+  },
+  {
+    "revision": "713fa458c05c7daa82b073fc12e56427",
+    "url": "/google-note/index.html"
+  }
+];
